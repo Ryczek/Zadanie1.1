7c48de3bde912cb283e1ecf2fcf4550b3788c6eb
Ryczek <miryczkowski@gmail.com>
2020-03-31 17:20:17 +0200

    Zadanie1

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 
 app = FastAPI()
 
 @app.get("/")
-def root():
-    return {"message": "Kocham Zuzię <3"}
+def Hello_World():
+    return {"message": "Hello World during the coronavirus pandemic!"}
+
+@app.get("/method")
+def method1(request: Request):
+    used_method=request.method
+    return {"method": used_method}
+
+@app.post("/method")
+def method2(request: Request):
+    used_method=request.method
+    return {"method": used_method}
